e3a8b83d3d49a72bcad7d0ea3bd642831edd2825
Ciro Neto <ciro.neto16@gmail.com>
2022-02-23 18:27:53 -0300

    initial commit - create project

---
+++ src/test/java/dev/cironeto/valhallaproject/ValhallaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cironeto.valhallaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValhallaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
