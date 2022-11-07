ce917545684f8fce5f0cac5a6b241bdf8dc41c52
Ciro <ciro.neto16@gmail.com>
2022-11-07 20:07:30 -0300

    Encode password before save

--- access-control-service/src/main/java/dev/cironeto/accesscontrolservice/config/SecurityConfig.java
+++ access-control-service/src/main/java/dev/cironeto/accesscontrolservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package dev.cironeto.accesscontrolservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -51,5 +53,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- access-control-service/src/main/java/dev/cironeto/accesscontrolservice/service/AppUserService.java
+++ access-control-service/src/main/java/dev/cironeto/accesscontrolservice/service/AppUserService.java
@@ -4,6 +4,8 @@ import dev.cironeto.accesscontrolservice.dto.UserPostRequestBody;
 import dev.cironeto.accesscontrolservice.model.AppUser;
 import dev.cironeto.accesscontrolservice.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,6 +15,7 @@ import java.util.UUID;
 public class AppUserService {
 
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public AppUser saveUser(UUID keycloakId, UserPostRequestBody userDto){
         AppUser user = new AppUser();
@@ -20,7 +23,7 @@ public class AppUserService {
         user.setEmail(userDto.getEmail());
         user.setFirstName(userDto.getFirstName());
         user.setLastName(userDto.getLastName());
-        user.setPassword(userDto.getPassword());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
         AppUser savedUser = appUserRepository.save(user);
         return savedUser;
     }
